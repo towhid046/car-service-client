9644bb45b321fcd435079d3763613985ab053701
towhid046 <towhidmorol46@gmail.com>
2024-04-30 09:55:45 +0600

    added about us section in home page

--- src/components/unique/AboutUs/AboutUs.jsx
+++ src/components/unique/AboutUs/AboutUs.jsx
@@ -1,10 +1,24 @@
 
-import 
+import personImg from '../../../assets/images/about_us/person.jpg'
+import partsImg from '../../../assets/images/about_us/parts.jpg'
+import SectionHeading from './../../shared/SectionHeading/SectionHeading';
 const AboutUs = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='py-12 flex justify-between gap-20 lg:flex-row flex-col '>
+            <div className='flex-1 relative'>
+                <img src={personImg} alt="Person Image" />
+                <img className='lg:absolute lg:mt-0 mt-6 border-white border-2 lg:w-96 -bottom-8 -right-8' src={partsImg} alt="Parts Image" />
+            </div>
+            <div className='flex-1 space-y-6'>
+                <SectionHeading
+                name='About Us'
+                title='We are qualified & of experience in this field'
+                description={`There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable`}
+                />
+                <p>The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <button className='btn btn-error'>Get More Info</button>
+            </div>
+        </section>
     );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from './../../components/unique/Hero/Hero';
+import AboutUs from './../../components/unique/AboutUs/AboutUs';
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <AboutUs/>
         </div>
     );
 };
